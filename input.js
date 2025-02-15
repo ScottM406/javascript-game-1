73871794009f4d60a40ed36c22fdbc59b2ff6e53
Scott <Scottyjmt13@gmail.com>
2025-02-15 14:15:26 -0700

    part 2 - controlling FPS.

--- input.js
+++ input.js
@@ -3,24 +3,24 @@ export class InputHandler {
     this.keys = [];
     window.addEventListener('keydown', e => {
       if (( e.key === 's' || 
-            e.key === 'w' || 
+            e.key === 'w' ||
             e.key === 'a' ||
             e.key === 'd' ||
             e.key === ' '
       ) && this.keys.indexOf(e.key) === -1){
         this.keys.push(e.key);
       }
-      // console.log(e.key, this.keys);
+      console.log(e.key, this.keys);
     });
     window.addEventListener('keyup', e => {
-      if (  e.key === 's' || 
+      if (  e.key === 's' ||
             e.key === 'w' ||
             e.key === 'a' ||
             e.key === 'd' ||
             e.key === ' ' ){
           this.keys.splice(this.keys.indexOf(e.key), 1);
         }
-        // console.log(e.key, this.keys);
+        console.log(e.key, this.keys);
     });
   }
 }
