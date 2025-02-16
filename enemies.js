7b1604a7b33cc2bd80c7b74032b0f6793f8a7229
Scott <Scottyjmt13@gmail.com>
2025-02-16 12:21:45 -0700

    add ground enemies

--- enemies.js
+++ enemies.js
@@ -49,7 +49,18 @@ export class FlyingEnemy extends Enemy {
 }
 
 export class GroundEnemy extends Enemy {
-
+  constructor(game){
+    super();
+    this.game = game;
+    this.width = 60;
+    this.height = 87;
+    this.x = this.game.width;
+    this.y = this.game.height - this.height - this.game.groundMargin;
+    this.image = document.getElementById('enemy-ground');
+    this.speedX = 0;
+    this.speedY = 0;
+    this.maxFrame = 1;
+  }
 }
 
 export class ClimbingEnemy extends Enemy {
--- main.js
+++ main.js
@@ -47,6 +47,9 @@ window.addEventListener('load', () => {
       })
     }
     addEnemy(){
+      if (this.speed > 0 && Math.random() <0.65){
+        this.enemies.push(new GroundEnemy(this))
+      } 
       this.enemies.push(new FlyingEnemy(this))
       console.log(this.enemies)
     }
